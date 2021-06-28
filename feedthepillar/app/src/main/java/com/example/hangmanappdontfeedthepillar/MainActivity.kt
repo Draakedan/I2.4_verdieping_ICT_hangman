3794bea451d50ae00065c8325c0dbd8e33011792
draakedan <anastasia_h@ziggo.nl>
2021-06-28 16:24:27 +0200

    [Add] android studio project

---
+++ feedthepillar/app/src/main/java/com/example/hangmanappdontfeedthepillar/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hangmanappdontfeedthepillar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.main_fragment_holder, TitleFragment()).commit()
+    }
+}
